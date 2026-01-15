57d6a1fe32f31b38e8ed8491f98420a4fb23ce38
Rob Johansen <rob.johansen@gmail.com>
2026-01-14 23:02:04 -0700

    gh-35 | Add the beginnings of keyboard shortcut detection

--- src/app/components/Keyboard/Store.ts
+++ src/app/components/Keyboard/Store.ts
@@ -2,14 +2,9 @@
 
 import { makeAutoObservable } from 'mobx'
 
+import { Modifier } from '@/types/Shortcut'
 import type { RootStore } from '@/app/RootStore'
 import type { Textile } from '@/types/Textile'
-
-/* Modifiers */
-let ALT = false
-let CTRL = false
-let META = false
-let SHIFT = false
 
 export class KeyboardStore {
   root: RootStore
@@ -28,20 +23,67 @@ export class KeyboardStore {
   }
 
   onKeyDown = (event: KeyboardEvent) => {
-    if (event.key === 'Alt') ALT = true
-    if (event.key === 'Control') CTRL = true
-    if (event.key === 'Meta') META = true
-    if (event.key === 'Shift') SHIFT = true
+    for (const textile of this.textiles) {
+      if (!textile.keyboard) continue
 
-    if (META && SHIFT && event.key !== 'Meta' && event.key !== 'Shift') {
-      console.log(`⌘+⇧+${event.key}`)
+      const alt = event.altKey
+      const ctrl = event.ctrlKey
+      const meta = event.metaKey
+      const shift = event.shiftKey
+
+      let modCount = 0
+      if (alt) modCount++
+      if (ctrl) modCount++
+      if (meta) modCount++
+      if (shift) modCount++
+
+      const firstMod1 = textile.keyboard.first.mod1
+      const firstMod2 = textile.keyboard.first.mod2
+      const firstKey = textile.keyboard.first.key
+
+      const secondMod1 = textile.keyboard.second?.mod1
+      const secondMod2 = textile.keyboard.second?.mod2
+      const secondKey = textile.keyboard.second?.key
+
+      if (
+        alt && firstMod1 === Modifier.Alt ||
+        ctrl && firstMod1 === Modifier.Control ||
+        meta && firstMod1 === Modifier.Meta
+      ) {
+        // The first mod 1 is down
+        if (firstMod2) {
+          if (
+            alt && firstMod2 === Modifier.Alt ||
+            ctrl && firstMod2 === Modifier.Control ||
+            meta && firstMod2 === Modifier.Meta ||
+            shift && firstMod2 === Modifier.Shift
+          ) {
+            // The first mod 2 is down
+            if (!textile.keyboard.second) {
+              if (event.key.toUpperCase() === firstKey) {
+                console.log(`Keyboard shortcut for ${textile.name}!`)
+              }
+            } else {
+              // Start the timer...or?
+            }
+          }
+        } else {
+          if (modCount !== 1) continue
+
+          // No other modifier is down
+          if (!textile.keyboard.second) {
+            if (event.key.toUpperCase() === firstKey) {
+              console.log(`Keyboard shortcut for ${textile.name}!`)
+            }
+          } else {
+            // Start the timer...or?
+          }
+        }
+      }
     }
   }
 
   onKeyUp = (event: KeyboardEvent) => {
-    if (event.key === 'Alt') ALT = false
-    if (event.key === 'Control') CTRL = false
-    if (event.key === 'Meta') META = false
-    if (event.key === 'Shift') SHIFT = false
+    // TODO: Stop the timer...
   }
 }
