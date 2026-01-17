8281178bcb14de978a575f6816b600988480b3fb
Rob Johansen <rob.johansen@gmail.com>
2026-01-17 13:20:15 -0700

    gh-35 | Add initial keyboard shortcut logic

--- src/app/components/Keyboard/Store.ts
+++ src/app/components/Keyboard/Store.ts
@@ -6,6 +6,11 @@ import { Modifier } from '@/types/Shortcut'
 import type { RootStore } from '@/app/RootStore'
 import type { Textile } from '@/types/Textile'
 
+let waitingMod1 = ''
+let waitingMod2 = ''
+let waitingKey = ''
+let timer: NodeJS.Timeout | undefined
+
 export class KeyboardStore {
   root: RootStore
 
@@ -23,19 +28,20 @@ export class KeyboardStore {
   }
 
   onKeyDown = (event: KeyboardEvent) => {
+    const alt = event.altKey
+    const ctrl = event.ctrlKey
+    const meta = event.metaKey
+    const shift = event.shiftKey
+    const key = event.key.toUpperCase()
+
+    let modCount = 0
+    if (alt) modCount++
+    if (ctrl) modCount++
+    if (meta) modCount++
+    if (shift) modCount++
+
     for (const textile of this.textiles) {
       if (!textile.keyboard) continue
-
-      const alt = event.altKey
-      const ctrl = event.ctrlKey
-      const meta = event.metaKey
-      const shift = event.shiftKey
-
-      let modCount = 0
-      if (alt) modCount++
-      if (ctrl) modCount++
-      if (meta) modCount++
-      if (shift) modCount++
 
       const firstMod1 = textile.keyboard.first.mod1
       const firstMod2 = textile.keyboard.first.mod2
@@ -46,37 +52,101 @@ export class KeyboardStore {
       const secondKey = textile.keyboard.second?.key
 
       if (
-        alt && firstMod1 === Modifier.Alt ||
-        ctrl && firstMod1 === Modifier.Control ||
-        meta && firstMod1 === Modifier.Meta
+        textile.keyboard.second &&
+        waitingMod1 === firstMod1 &&
+        waitingMod2 === firstMod2 &&
+        waitingKey === firstKey
       ) {
-        // The first mod 1 is down
-        if (firstMod2) {
-          if (
-            alt && firstMod2 === Modifier.Alt ||
-            ctrl && firstMod2 === Modifier.Control ||
-            meta && firstMod2 === Modifier.Meta ||
-            shift && firstMod2 === Modifier.Shift
-          ) {
-            // The first mod 2 is down
-            if (!textile.keyboard.second) {
-              if (event.key.toUpperCase() === firstKey) {
-                console.log(`Keyboard shortcut for ${textile.name}!`)
+        console.log(`1 ${textile.name}`)
+        console.log('waitingMod1:', waitingMod1)
+        console.log('waitingMod2:', waitingMod2)
+        console.log('waitingKey:', waitingKey)
+        // This textile has a second sequence, and its first sequence was pressed within the time window.
+        if (
+          alt && secondMod1 === Modifier.Alt ||
+          ctrl && secondMod1 === Modifier.Control ||
+          meta && secondMod1 === Modifier.Meta
+        ) {
+          console.log(`2 ${textile.name}`)
+          // The mod 1 of the second sequence is down.
+          if (secondMod2) {
+            console.log(`3 ${textile.name}`)
+            // This textile's shortcut has a mod 2 in its second sequence.
+            if (
+              alt && secondMod2 === Modifier.Alt ||
+              ctrl && secondMod2 === Modifier.Control ||
+              meta && secondMod2 === Modifier.Meta ||
+              shift && secondMod2 === Modifier.Shift
+            ) {
+              console.log(`4 ${textile.name}`)
+              // The mod 2 of the second sequence is down.
+              if (key === secondKey) {
+                console.log(`5 ${textile.name}`)
+                // We've matched the shortcut of a textile that has two sequences, and two modifiers in its second sequence.
+                console.log(`Matched "${textile.name}"!`)
+                this.reset()
+              } else {
+                console.log(`key: ${key}`)
+                console.log(`secondKey: ${secondKey}`)
               }
-            } else {
-              // Start the timer...or?
-            }
-          }
-        } else {
-          if (modCount !== 1) continue
-
-          // No other modifier is down
-          if (!textile.keyboard.second) {
-            if (event.key.toUpperCase() === firstKey) {
-              console.log(`Keyboard shortcut for ${textile.name}!`)
             }
           } else {
-            // Start the timer...or?
+            // The second sequence of this textile's shortcut does not have two modifiers.
+            // If two or more modifiers are being pressed, we can immediately rule it out.
+            if (modCount !== 1) continue
+
+            if (key === secondKey) {
+              // We've matched the shortcut of a textile that has two sequences, and one modifier in its second sequence.
+              console.log(`Matched "${textile.name}"!`)
+              this.reset()
+            }
+          }
+        }
+      } else {
+        // This textile only has one sequence, or has two sequences but is not yet waiting for its second sequence.
+        if (
+          alt && firstMod1 === Modifier.Alt ||
+          ctrl && firstMod1 === Modifier.Control ||
+          meta && firstMod1 === Modifier.Meta
+        ) {
+          // The mod 1 of the first sequence is down.
+          if (firstMod2) {
+            // This textile's shortcut has a mod 2 in its first sequence.
+            if (
+              alt && firstMod2 === Modifier.Alt ||
+              ctrl && firstMod2 === Modifier.Control ||
+              meta && firstMod2 === Modifier.Meta ||
+              shift && firstMod2 === Modifier.Shift
+            ) {
+              // The mod 2 of the first sequence is down.
+              if (key === firstKey) {
+                // The key of the first sequence is being pressed.
+                if (textile.keyboard.second) {
+                  // This textile's shortcut has a second sequence, so we wait for it.
+                  this.wait(firstMod1, firstMod2, firstKey)
+                } else {
+                  // We've matched the shortcut of a textile that has one sequence, and two modifiers in that sequence.
+                  console.log(`Matched "${textile.name}"!`)
+                  this.reset()
+                }
+              }
+            }
+          } else {
+            // The first sequence of this textile's shortcut does not have two modifiers.
+            // If two or more modifiers are being pressed, we can immediately rule it out.
+            if (modCount !== 1) continue
+
+            if (key === firstKey) {
+              // The key of the first sequence is down.
+              if (textile.keyboard.second) {
+                // This textile's shortcut has a second sequence, so we wait for it.
+                this.wait(firstMod1, firstMod2, firstKey)
+              } else {
+                // We've matched the shortcut of a textile that has one sequence, and one modifier in that sequence.
+                console.log(`Matched "${textile.name}"!`)
+                this.reset()
+              }
+            }
           }
         }
       }
@@ -84,6 +154,22 @@ export class KeyboardStore {
   }
 
   onKeyUp = (event: KeyboardEvent) => {
-    // TODO: Stop the timer...
+    // TODO: What should actually happen here?
+  }
+
+  reset = () => {
+    clearTimeout(timer)
+    waitingMod1 = ''
+    waitingMod2 = ''
+    waitingKey = ''
+  }
+
+  wait = (firstMod1: string, firstMod2: string | undefined, firstKey: string) => {
+    clearTimeout(timer)
+    timer = setTimeout(this.reset, 2000)
+
+    waitingMod1 = firstMod1
+    waitingMod2 = firstMod2 ?? ''
+    waitingKey = firstKey
   }
 }
